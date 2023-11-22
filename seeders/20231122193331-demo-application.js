29cbf4e66168e1bb8b6dc667443724c19063aab1
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-22 16:31:34 -0400

    Defined application seeder

--- seeders/20231122193331-demo-application.js
+++ seeders/20231122193331-demo-application.js
@@ -1,3 +1,4 @@
+// ###C:\Users\ricar\repos\Build_Link-Grillo\seeders\20231122193331-demo-application.js
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
@@ -5,21 +6,32 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     /**
      * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+     * Seeding the Application table with sample data.
+     */
+    await queryInterface.bulkInsert('Applications', [{
+      userId: 1, // Assuming a user with ID 1 exists
+      postId: 1, // Assuming a post with ID 1 exists
+      coverLetter: 'I am very interested in this position and believe my skills are a perfect match.',
+      resume: 'path/to/resume1.pdf', // Assuming this is a URL or path to a file
+      status: 'pending', // The current status of the application
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      userId: 2, // Assuming a user with ID 2 exists
+      postId: 2, // Assuming a post with ID 2 exists
+      coverLetter: 'With my extensive experience, I am confident in my ability to contribute to the team.',
+      resume: 'path/to/resume2.pdf', // Assuming this is a URL or path to a file
+      status: 'pending', // The current status of the application
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
   },
 
   async down (queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
+     * Useful for removing the test data.
      */
+    await queryInterface.bulkDelete('Applications', null, {});
   }
 };

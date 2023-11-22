88e00178cb8e9e851df161b64417ed6b6285fe0c
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-22 16:16:40 -0400

    Added detailed comments to models

--- models/user.js
+++ models/user.js
@@ -7,52 +7,56 @@ module.exports = (sequelize, DataTypes) => {
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
+     * Time to define relationships for the User model.
      */
     static associate(models) {
       // One-to-Many with Post
+      // A user can create many posts but each post belongs to one user.
       User.hasMany(models.Post, {
         foreignKey: 'user_id',
-        as: 'posts',
-        onDelete: 'CASCADE'
+        as: 'posts', // Giving an alias for easier access
+        onDelete: 'CASCADE' // If a user is deleted, their posts should also be removed.
       });
 
       // One-to-Many with Comment
+      // A user can make many comments, but each comment is linked to one user.
       User.hasMany(models.Comment, {
         foreignKey: 'user_id',
-        as: 'comments',
-        onDelete: 'CASCADE'
+        as: 'comments', // Alias for accessing a user's comments
+        onDelete: 'CASCADE' // Deleting a user should delete their comments.
       });
 
       // One-to-Many with Application
+      // Users can have many applications, but each application is tied to one user.
       User.hasMany(models.Application, {
         foreignKey: 'user_id',
-        as: 'applications',
-        onDelete: 'CASCADE'
+        as: 'applications', // Alias to access applications made by the user
+        onDelete: 'CASCADE' // If a user is deleted, their applications should be deleted too.
       });
 
       // Self-Referential Many-to-Many for Messages
-      // Assuming you have a Message model where 'sender_id' and 'receiver_id' are the fields
+      // Users can send and receive messages. This sets up two relations for the same user.
       User.hasMany(models.Message, {
         foreignKey: 'sender_id',
-        as: 'sentMessages'
+        as: 'sentMessages' // Messages sent by the user
       });
       User.hasMany(models.Message, {
         foreignKey: 'receiver_id',
-        as: 'receivedMessages'
+        as: 'receivedMessages' // Messages received by the user
       });
     }
   }
 
   User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    profile_picture: DataTypes.STRING,
-    bio: DataTypes.TEXT
+    username: DataTypes.STRING, // The user's username
+    email: DataTypes.STRING, // Email address
+    password: DataTypes.STRING, // User's password, should be hashed in real-world scenarios
+    profile_picture: DataTypes.STRING, // URL or path to the profile picture
+    bio: DataTypes.TEXT // Short biography or description of the user
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'User', // Naming the model
   });
 
-  return User;
+  return User; // Returning the defined User class
 };

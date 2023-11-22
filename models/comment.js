95053969b7c820a70e5ee25b76dc1da64982ec79
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-22 15:26:12 -0400

    Generated and defined Comment model

---
+++ models/comment.js
@@ -0,0 +1,53 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Comment to User (Many-to-One)
+      Comment.belongsTo(models.User, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE'
+      });
+
+      // Comment to Post (Many-to-One)
+      Comment.belongsTo(models.Post, {
+        foreignKey: 'postId',
+        onDelete: 'CASCADE'
+      });
+    }
+  }
+
+  Comment.init({
+    text: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Posts',
+        key: 'id'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Comment',
+  });
+
+  return Comment;
+};

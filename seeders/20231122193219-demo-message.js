d4627b3960397a9b3f201a21c45e67db6c8429f6
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-22 16:29:15 -0400

    Defined post seeder

--- seeders/20231122193219-demo-message.js
+++ seeders/20231122193219-demo-message.js
@@ -1,4 +1,3 @@
-// ###C:\Users\ricar\repos\Build_Link-Grillo\seeders\20231122193219-demo-message.js
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
--- seeders/20231122193258-demo-post.js
+++ seeders/20231122193258-demo-post.js
@@ -5,21 +5,34 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     /**
      * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+     * Seeding the Post table with sample data.
+     */
+    await queryInterface.bulkInsert('Posts', [{
+      title: 'Senior Software Engineer',
+      description: 'Looking for an experienced software engineer to join our team.',
+      location: 'New York, NY',
+      salary: 120000,
+      company: 'TechCorp',
+      user_id: 1, // Assuming a user with ID 1 exists
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      title: 'Graphic Designer',
+      description: 'Seeking a creative graphic designer for our marketing team.',
+      location: 'San Francisco, CA',
+      salary: 90000,
+      company: 'CreativeDesigns',
+      user_id: 2, // Assuming a user with ID 2 exists
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
   },
 
   async down (queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
+     * Useful for removing the test data.
      */
+    await queryInterface.bulkDelete('Posts', null, {});
   }
 };

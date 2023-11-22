926dd4a906da3adad92d0eb7156d6d4deb007b17
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-22 16:27:11 -0400

    Defined message seeder

--- seeders/20231122193219-demo-message.js
+++ seeders/20231122193219-demo-message.js
@@ -1,3 +1,4 @@
+// ###C:\Users\ricar\repos\Build_Link-Grillo\seeders\20231122193219-demo-message.js
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
@@ -5,21 +6,30 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     /**
      * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+     * Seeding the Message table with sample messages.
+     */
+    await queryInterface.bulkInsert('Messages', [{
+      senderId: 1, // Assuming user with ID 1 is the sender
+      receiverId: 2, // Assuming user with ID 2 is the receiver
+      content: 'Hello, how are you?',
+      timestamp: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      senderId: 2, // Assuming user with ID 2 is the sender
+      receiverId: 1, // Assuming user with ID 1 is the receiver
+      content: 'I am fine, thank you!',
+      timestamp: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
   },
 
   async down (queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
+     * Useful for removing the test data.
      */
+    await queryInterface.bulkDelete('Messages', null, {});
   }
 };
